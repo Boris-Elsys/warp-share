be93e946abc45d23ab42f6f3776c3535f9077b52
Alis <71932909+Alis-elsys@users.noreply.github.com>
2023-04-17 02:43:55 +0300

    Create Login.js

---
+++ login2/src/components/Login.js
@@ -0,0 +1,146 @@
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import Home from "./Home";
+import AuthService from "../services/auth_service";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="invalid-feedback d-block">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+const Login = (props) => {
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    setMessage("");
+    setLoading(true);
+
+    form.current.validateAll();
+
+    if (checkBtn.current.context._errors.length === 0) {
+      AuthService.login(email, password).then(
+        () => {
+          navigate("/Home");
+          window.location.reload();
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          setLoading(false);
+          setMessage(resMessage);
+        }
+      );
+    } else {
+      setLoading(false);
+    }
+  };
+
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (response.ok) {
+            const token = await response.json();
+            document.cookie = `token=${token}; path=/; expires=${new Date(Date.now() + 86400e3)}; secure`;
+            localStorage.setItem('token', token);
+            //Redirect to home page
+            props.onFormSwitch('Home');
+        } else {
+            // Display error message
+        }
+    };
+
+
+  return (
+    <div className="col-md-12">
+      <div className="card card-container">
+        <img
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+          alt="profile-img"
+          className="profile-img-card"
+        />
+
+        <Form onSubmit={handleLogin} ref={form}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <Input
+              type="email"
+              className="form-control"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              validations={[required]}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <Input
+              type="password"
+              className="form-control"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              validations={[required]}
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-primary btn-block" type="submit">Login</button>
+            {/* <button className="btn btn-primary btn-block" disabled={loading}>
+              {loading && (
+                <span className="spinner-border spinner-border-sm"></span>
+              )}
+              <span>Login</span>
+            </button> */}
+          </div>
+          <p>Don't have an account?</p>
+          <button className="btn btn-primary btn-block" onClick={() => props.onFormSwitch('Register')}>Register here</button>
+
+          {/* {message && (
+            <div className="form-group">
+              <div className="alert alert-danger" role="alert">
+                {message}
+              </div>
+            </div>
+          )}
+          <CheckButton style={{ display: "none" }} ref={checkBtn} /> */}
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
